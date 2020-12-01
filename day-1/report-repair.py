1ef9dd930061c7a0cee1941fcffc23e149796a0b
Vicki <vicki.langer@statalog.org>
2020-12-01 16:39:59 -0500

    more detail explaining puzzle

--- day-1/report-repair.py
+++ day-1/report-repair.py
@@ -1,5 +1,5 @@
 """
-report-repair.py: solve puzzle at https://adventofcode.com/2020/day/1
+report-repair.py: from input.text find the two entries that sum to 2020; solve puzzle at https://adventofcode.com/2020/day/1
 1 December 2020
 @Vicki_Langer
 """
