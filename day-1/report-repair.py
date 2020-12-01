fa106d13dd1bf980ca5797ea0bdcb844283311fc
Vicki <vicki.langer@statalog.org>
2020-12-01 16:41:17 -0500

    again fixing puzzle problem

--- day-1/report-repair.py
+++ day-1/report-repair.py
@@ -1,7 +1,5 @@
 """
-report-repair.py: from input.text find the two entries that sum to 2020; solve puzzle at https://adventofcode.com/2020/day/1
-1 December 2020
-@Vicki_Langer
+report-repair.py: from input.text find the two entries that sum to 2020 and then multiply those two numbers together; solve puzzle at https://adventofcode.com/2020/day/1
 """
 
 import text file with list of numbers
