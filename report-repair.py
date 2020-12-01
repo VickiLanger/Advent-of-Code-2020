64d1ded3c3c9ff2efd766adcd935749f9a9bc597
Vicki <vicki.langer@statalog.org>
2020-12-01 16:35:12 -0500

    set up docstrings

---
+++ report-repair.py
@@ -0,0 +1,5 @@
+"""
+report-repair.py: solve puzzle at https://adventofcode.com/2020/day/1
+1 December 2020
+@Vicki_Langer
+"""
---
+++ report-repair.rb
@@ -0,0 +1,7 @@
+"""
+report-repair.rb: solve puzzle at https://adventofcode.com/2020/day/1
+1 December 2020
+@Vicki_Langer
+"""
+
+
