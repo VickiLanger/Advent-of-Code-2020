622bd96c98027f4bf3de21e61baf5a244afc0471
Vicki <vicki.langer@statalog.org>
2020-12-02 16:13:25 -0500

    puzzle solved!

--- password-philosophy.py
+++ password-philosophy.py
@@ -1,7 +1,22 @@
 """
 password-philosophy.py: from day_2.txt find out how many passwords are valid according to their policies; solve puzzle at https://adventofcode.com/2020/day/2
 2 December 2020
-@Vicki_Langer
+@Vicki_Langer / @VickiLanger
 """
 
-read txt file
+# read text file
+
+
+def validate_passwords():
+    num_valid_passwords = 0
+    for line in open('inputs/day_2.txt').readlines():
+        lower_upper, character, password = line.strip('\n').split()
+        character = character[0]
+        lower, upper = lower_upper.split('-')
+        count = password.count(character)
+        if (count >= int(lower)) & (count <= int(upper)):
+            num_valid_passwords += 1
+            print(num_valid_passwords)
+
+
+validate_passwords()
