e3afc9e0b8b961acf09a5ba1ff119783c532448f
Vicki <vicki.langer@statalog.org>
2020-12-02 12:50:59 -0500

    fix docstring

--- report-repair.py
+++ report-repair.py
@@ -1,5 +1,5 @@
 """
-report-repair.py: from input.text find the two entries that sum to 2020 and then multiply those two numbers together; solve puzzle at https://adventofcode.com/2020/day/1
+report-repair.py: from input.txt find the two entries that sum to 2020 and then multiply those two numbers together; solve puzzle at https://adventofcode.com/2020/day/1
 1 December 2020
 @Vicki_Langer
 """
