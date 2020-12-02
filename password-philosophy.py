13c1ed61a2f8ba5129e72859a867557c11d42662
Vicki <vicki.langer@statalog.org>
2020-12-02 12:27:32 -0500

    initiate day 2 files

---
+++ password-philosophy.py
@@ -0,0 +1,5 @@
+"""
+password-philosophy.py: from day_2.txt find out how many passwords are valid according to their policies; solve puzzle at https://adventofcode.com/2020/day/1
+2 December 2020
+@Vicki_Langer
+"""
