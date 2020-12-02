28bc98920d522070f3d9d2500a7167072ca1ac8a
Vicki <vicki.langer@statalog.org>
2020-12-02 15:15:30 -0500

    solved puzzle!

--- password-philosophy.rb
+++ password-philosophy.rb
@@ -4,13 +4,18 @@ password-philosophy.rb: from day_2.txt find out how many passwords are valid acc
 @Vicki_Langer
 """
 
-read each line of txt file
+valid_passwords = 0
+
+passwords_with_policies = File.readlines("inputs/day_2.txt")
 
 # \d numerical digit
 # \w alphanumeric characters
 # * zero or more
 
-
-use regex to .match
-use .captures # returns array of matches
-decide if valid with .between?
+passwords_with_policies. each do |line|
+    low, high, character, password = line.match(/(\d*)-(\d*) (\w): (\w*)/).captures  # each () goes to the respective variable and is the captured in an array
+        if password.count(character).between?(low.to_i, high.to_i)
+            valid_passwords += 1
+        end
+    puts valid_passwords
+end
