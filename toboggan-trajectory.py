764231e0533400cb3f5b3b940fe6604d42ca098d
Vicki <vicki.langer@statalog.org>
2020-12-03 13:26:48 -0500

    some more pseudocode-ish

--- toboggan-trajectory.py
+++ toboggan-trajectory.py
@@ -5,11 +5,27 @@ toboggan-trajectory.py: from day_3.txt find out how many trees the sled would hi
 """
 
 
-read text file
+# read text file
+tree_map_section = open("inputs/day_3.txt", "r").read().splitlines()
 
-repeat tree grid (day_3.txt) to the right a bunch of times
+# repeat tree_map_section to the right a bunch of times
+tree_map_full = 
+
+move_right = 3
+move_down = 1
+while move_right and move_down:
+    tree = "#"
+    hit_tree_count = 0
+    if hit tree:
+        hit_tree_count += 1
+    print("Damn! You hit " + hit_tree_count + " trees! How are you still alive?!")
+
+
+"""
+repeat tree_map_section to the right a bunch of times
 tree = "#"
 move sled 3 to the right
 move sled 1 spot down
 count trees when moving right 3 and down 1
 print number of trees hit
+"""
