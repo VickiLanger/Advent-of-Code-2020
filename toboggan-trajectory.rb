c997c064134584ed287d5499bbcf295c86e78f98
Vicki <vicki.langer@statalog.org>
2020-12-03 11:52:25 -0500

    pseudocoded

--- toboggan-trajectory.rb
+++ toboggan-trajectory.rb
@@ -1,9 +1,15 @@
 """
-toboggan-trajectory.rb: from day_3.txt find out how many trees the sled would hit; solve puzzle at https://adventofcode.com/2020/day/3
+toboggan-trajectory.rb: from day_3.txt find out how many trees the sled would hit when moving right 3 and down 1; solve puzzle at https://adventofcode.com/2020/day/3
 3 December 2020
 @Vicki_Langer / @VickiLanger
 """
 
 
 read text file
-print answer
+
+repeat tree grid (day_3.txt) to the right a bunch of times
+tree = "#"
+move sled 3 to the right
+move sled 1 spot down
+count trees when moving right 3 and down 1
+print number of trees hit
